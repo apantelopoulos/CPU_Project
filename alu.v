1be50eb505abde43667e3e4604c450ffe6b63952
apantelopoulos <59971511+apantelopoulos@users.noreply.github.com>
2020-05-29 15:43:29 +0300

    Instr Change (2) and improve alu

--- alu.v
+++ alu.v
@@ -3,22 +3,38 @@ module alu
 input [15:0] instr,
 input [15:0] inreg1,
 input [15:0] inreg2,
-input carrryin,
+input [15:0] inp,
+input carryin,
 output [15:0] aluout,
 output carryout,
 output carryen,
 output wenout
 );
 
-
-
+reg [31:0] mul0;
+reg [31:0] mul1;
+reg [31:0] mul2;
+reg [31:0] mul3;
+reg [31:0] mul4;
+reg [31:0] mul5;
+reg [31:0] mul6;
+reg [31:0] mul7;
+reg [31:0] mul8;
+reg [31:0] mul9;
+reg [31:0] mul10;
+reg [31:0] mul11;
+reg [31:0] mul12;
+reg [31:0] mul13;
+reg [31:0] mul14;
+reg [31:0] mul15;
+reg [31:0] mul;
 
 reg [16:0] alusum;
-wire [7:0] op = [15:8] instr;
-wire [1:0] rd = [7:6] instr;
-wire [1:0] rm = [5:4] instr;
-wire [1:0] rn = [3:2] instr;
-wire carryen = instr[1];
+wire [7:0] op = instr[15:8];
+wire [1:0] rd = instr[7:6];
+wire [1:0] rm = instr[5:4];
+wire [1:0] rn = instr[3:2];
+wire carrynbl = instr[1];
 wire cin = instr[0]? carryin : 0;
 
 always @(*)
@@ -29,10 +45,36 @@ always @(*)
 			8'b11111001 : alusum = {1'b0,inreg1} - {1'b0,inreg2} + cin;
 			8'b11111010 : alusum = {1'b0,inreg1} + 1;
 			8'b11111011 : alusum = {1'b0,inreg1} - 1;
+			8'b11111100 : alusum = {1'b0,inp};
+			8'b11111101 :
+				begin
+					mul0 = inreg1[0] ? inreg2 : 0;
+					mul1 = inreg1[1] ? inreg2 << 1 : 0;
+					mul2 = inreg1[1] ? inreg2 << 2 : 0;
+					mul3 = inreg1[1] ? inreg2 << 3 : 0;
+					mul4 = inreg1[1] ? inreg2 << 4 : 0;
+					mul5 = inreg1[1] ? inreg2 << 5 : 0;
+					mul6 = inreg1[1] ? inreg2 << 6 : 0;
+					mul7 = inreg1[1] ? inreg2 << 7 : 0;
+					mul8 = inreg1[1] ? inreg2 << 8 : 0;
+					mul9 = inreg1[1] ? inreg2 << 9 : 0;
+					mul10 = inreg1[1] ? inreg2 << 10 : 0;
+					mul11 = inreg1[1] ? inreg2 << 11 : 0;
+					mul12 = inreg1[1] ? inreg2 << 12 : 0;
+					mul13 = inreg1[1] ? inreg2 << 13 : 0;
+					mul14 = inreg1[1] ? inreg2 << 14 : 0;
+					mul15 = inreg1[1] ? inreg2 << 15 : 0;
+					mul = mul0 + mul1 + mul2 + mul3 + mul4 + mul5 + mul6 + mul7 + mul8 + mul9 + mul10 + mul11 + mul12 + mul13 + mul14 + mul15; 
+					alusum = mul[16:0];				
+				end 
 			
 	endcase
 	
 	end
+
+assign alucout = alusum [16]; // carry bit from sum, or shift if OP = 011
+assign aluout = alusum [15:0];
+
 	
-	
+endmodule
 	
--- decoder.v
+++ decoder.v
@@ -16,8 +16,6 @@ module decoder
 	output wrenram,
 	output sel_mux_din_reg,
 	output sel_mux_lds,
-	output sel_mux_din_reg2,
-	output sel_mux_output,
 	output sel_mux_din_ram
 );
 
@@ -31,22 +29,20 @@ wire jeq = instr[15]&instr[14]&~instr[13];
 
 wire jmp = instr[15]&instr[14]&instr[13]&~instr[12]&~instr[11];
 wire pls = instr[15]&instr[14]&instr[13]&~instr[12]&instr[11];
-wire otp = instr[15]&instr[14]&instr[13]&instr[12]&~instr[11];
+wire stp = instr[15]&instr[14]&instr[13]&instr[12]&~instr[11];
 
 wire regwork = instr[15]&instr[14]&instr[13]&instr[12]&instr[11];
-wire inp = regwork&~instr[10]&instr[9]&~instr[8];
 
-assign extra = (lda | ldn | stn | adn | otp);
+
+assign extra = (lda | ldn | stn | adn);
 assign extra2 = (ldn | adn);
-assign pc_cnt_en = exec1&~(jeq&eq | jmp) | exec1&(ldi|pls|sta|inp) | exec2&(lda|stn|otp) | exec3&(ldn|adn) | exec1&regwork;
+assign pc_cnt_en = ~stp&(exec1&~(jeq&eq | jmp) | exec1&(ldi|pls|sta) | exec2&(lda|stn) | exec3&(ldn|adn) | exec1&regwork);
 assign pc_sload = exec1&(jeq&eq | jmp);
-assign wrenreg = exec2&lda | exec3&ldn | exec3&adn | exec1&ldi | exec1&inp;
+assign wrenreg = exec2&lda | exec3&ldn | exec3&adn | exec1&ldi;
 assign sel_mux_adr_rom = exec1&jeq&eq | exec1&jmp;
-assign sel_mux_adr_ram = exec2&lda | exec2&ldn | exec3&ldn | exec2&stn | exec2&adn | exec3&adn | exec2&otp;
+assign sel_mux_adr_ram = exec2&lda | exec2&ldn | exec3&ldn | exec2&stn | exec2&adn | exec3&adn;
 assign wrenram = sta&exec1 | stn&exec2 | pls&exec1;
 assign sel_mux_din_reg = adn;
 assign sel_mux_lds = ldi;
-assign sel_mux_din_reg2 = inp;
-assign sel_mux_output = otp&exec2;
 assign sel_mux_din_ram = pls;
 endmodule
